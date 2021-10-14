de72e29e48ef44f40c7eab9ed4ba856b9e044859
beshiralkhadra <beshiralkhadra@gmail.com>
2021-10-14 14:28:13 +0300

    fourthUpload

--- main6.js
+++ main6.js
@@ -2,13 +2,18 @@ let c = document.querySelector("#btn");
 let audio = document.querySelector("div");
 let fName = document.querySelector("#fname");
 fName.style.cssText = "margin-left:1em;margin-top:1em";
-audio.style.cssText = "margin:1em;border:solid 1px;width:80%";
+
 c.addEventListener("click", addSongs);
 function addSongs() {
-  let b = document.createElement("p");
+  let b = document.createElement("ul");
+  let z = document.createElement("li");
+  b.appendChild(z);
 
   let e = document.createTextNode("new song");
-  b.appendChild(e);
+
+  z.appendChild(e);
   audio.appendChild(b);
+  audio.style.cssText =
+    "margin-left:1em;margin-bottom:.5em;margin-top:.5em;border:solid 1px;padding:.5em;width:50%;background:#ECECEC";
 }
 addSongs();
